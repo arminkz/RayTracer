c484bb72676e1db061f319102cd9f71de9ef308e
arminkz <arminkz@live.com>
2026-04-29 20:23:40 -0600

    chore: add precompiled headers

--- src/pch.h
+++ src/pch.h
@@ -11,9 +11,6 @@
 #include "spdlog/spdlog.h"
 
 // [GLM]
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_ENABLE_EXPERIMENTAL
 #include "glm/glm.hpp"
 #include "glm/gtc/constants.hpp"
 #include "glm/gtc/matrix_transform.hpp"
